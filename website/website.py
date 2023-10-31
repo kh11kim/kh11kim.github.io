d90f01f790f245b433543db67ecf1f472b0df37d
Kanghyun Kim <kh11kim@kaist.ac.kr>
2023-10-31 11:16:24 +0900

    rearrange things

--- website/website.py
+++ website/website.py
@@ -3,12 +3,140 @@ import os
 import reflex as rx
 
 from .states import State
+from .pages.research import *
 
-def index():
-    return rx.center(
-        rx.box("hello world")
+def h_spacing(px):
+    return rx.box(h=f"{px}px")
+
+def name():
+    return rx.vstack(
+        rx.heading(
+            "Kanghyun Kim", font_size="2em"
+        ),
+        rx.text(
+            "kh11kim at kaist.ac.kr",
+            font_family="courier new",
+            font_size="0.9em",
+            color="gray"
+        )
     )
 
-app = rx.App()
-app.add_page(index, route="/")
+def intro():
+    return rx.vstack(
+        rx.text(
+            "Contents will be uploaded soon..! \
+             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, \
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book."),
+        rx.text(
+            "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. \
+            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+    )
+
+def aboutme():
+    return rx.grid(
+        rx.grid_item(
+            name(),
+            h_spacing(20),
+            intro(),
+            row_span=3, col_span=5
+        ),
+        rx.grid_item(
+            "picture",
+            row_span=3, col_span=2, bg="lightgreen"
+        ),
+        template_rows="repeat(3, 1fr)",
+        template_columns="repeat(7, 1fr)",
+        h="260px",
+        gap=4,
+    )
+
+def link_buttons():
+    return rx.center(rx.button_group(
+        rx.button("CV", size="sm"),
+        rx.text("/", font_size="20px", color="lightgray"),
+        rx.button("Google Scholar", size="sm"),
+        rx.text("/", font_size="20px", color="lightgray"),
+        rx.button("Github", size="sm"),
+        rx.text("/", font_size="20px", color="lightgray"),
+        rx.button("LinkedIn", size="sm"),
+        variant="ghost",
+        font_family="courier new",
+        spacing=8
+    ))
+
+def research():
+    return rx.box(
+        rx.heading("Research", size="md"),
+        h_spacing(15),
+        "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. \
+        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    )
+
+def publications():
+    return rx.box(
+        rx.heading("Publications", size="md"),
+        h_spacing(15),
+        publication_card(),
+        publication_card(),
+        publication_card(),
+        publication_card()
+    )
+
+def publication_card():
+    return rx.box(rx.grid(
+            rx.grid_item(
+                "figure",
+                row_span=3, col_span=2, bg="lightgreen"
+            ),
+            rx.grid_item(
+                rx.heading("Title of the paper", size="sm"),
+                rx.text("author list"),
+                rx.text("Name of the conference/journal", as_="i"),
+                rx.text("some useful links"),
+                h_spacing(10),
+                rx.text("Very brief summary of the paper. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."),
+                row_span=3, col_span=6
+            ),
+            template_rows="repeat(3, 1fr)",
+            template_columns="repeat(8, 1fr)",
+            h="170px",
+            gap=4,
+        ),
+        h_spacing(15),
+    )
+
+def license():
+    return rx.vstack(
+        rx.box(h="10px"),
+        rx.box(
+            "Kanghyun Kim ⓒ 2023",
+            font_family="courier new",
+            font_size="0.85em",
+            color="gray"
+        ),
+        rx.box(h="10px"),
+)
+
+def index():
+    return rx.center(rx.box(
+        h_spacing(100),
+        aboutme(),
+        h_spacing(20),
+        link_buttons(),
+        h_spacing(80),
+        research(),
+        h_spacing(50),
+        publications(),
+        h_spacing(50),
+        license(),
+        width="70%",
+    ))
+
+style = {
+    "font_family": "Comic Sans MS",
+    "font_size": "15px",
+}
+
+app = rx.App(style=style)
+app.add_page(index, route="/", title="Kanghyun Kim")
 app.compile()
