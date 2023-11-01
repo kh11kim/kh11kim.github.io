8649c914de34666c601bd21e583dfd69301ea3b8
Kanghyun Kim <kh11kim@kaist.ac.kr>
2023-11-01 13:24:14 +0900

    mobile compatibility

--- website/website.py
+++ website/website.py
@@ -21,53 +21,55 @@ def name():
 
 def intro():
     return rx.vstack(
-        rx.text(
-            "Contents will be uploaded soon..! \
-             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, \
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book."),
-        rx.text(
-            "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. \
-            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+        rx.text("Contents will be uploaded soon..! \
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, \
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book."),
+        rx.text("It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. \
+        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
     )
 
 def aboutme():
-    return rx.grid(
-        rx.grid_item(
+    return rx.hstack(
+        rx.vstack(
             name(),
-            h_spacing(20),
+            h_spacing(30),
             intro(),
-            row_span=3, col_span=5
+            max_width="500px"
         ),
-        rx.grid_item(
-            "picture",
-            row_span=3, col_span=2, bg="lightgreen"
+        rx.center(
+            rx.image(
+                src="/image/20231002_145255.jpg",
+                border_radius="50%"
+            ),
+            max_width="30%",
         ),
-        template_rows="repeat(3, 1fr)",
-        template_columns="repeat(7, 1fr)",
-        h="260px",
-        gap=4,
+        spacing="1em",
     )
 
 def link_buttons():
+    def divider():
+        return rx.text("|", font_size="20px", color="lightgray")
     return rx.center(rx.button_group(
-        rx.button("CV", size="sm"),
-        rx.text("/", font_size="20px", color="lightgray"),
-        rx.button("Google Scholar", size="sm"),
-        rx.text("/", font_size="20px", color="lightgray"),
-        rx.button("Github", size="sm"),
-        rx.text("/", font_size="20px", color="lightgray"),
-        rx.button("LinkedIn", size="sm"),
+        rx.button("CV"),
+        divider(),
+        rx.button("Google Scholar"),
+        divider(),
+        rx.button("Github"),
+        divider(),
+        rx.button("LinkedIn"),
         variant="ghost",
-        font_family="courier new",
-        spacing=8
+        #font_family="courier new",
+        #font_size="0.85em"
+        size="100px"
+        #spacing=8
     ))
 
 def research():
-    return rx.box(
-        rx.heading("Research", size="md"),
-        h_spacing(15),
-        "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. \
-        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    return rx.card(
+        rx.box("It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. \
+         It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        ),
+        header=rx.heading("Research", size="md"),
     )
 
 
@@ -84,25 +86,29 @@ def license():
 )
 
 def index():
-    return rx.center(rx.box(
-        h_spacing(100),
-        aboutme(),
-        h_spacing(20),
-        link_buttons(),
-        h_spacing(80),
-        research(),
-        h_spacing(50),
-        publications(),
-        h_spacing(50),
-        license(),
-        width="70%",
-    ))
+    return rx.center(
+            rx.vstack(
+                h_spacing(100),
+                aboutme(),
+                h_spacing(20),
+                link_buttons(),
+                h_spacing(80),
+                research(),
+                h_spacing(50),
+                publications(),
+                h_spacing(50),
+                license(),
+                font_family="Nunito Sans",
+                font_size="0.9em",
+                max_width="95%",
+            ),
+            spacing="2em",
+        )
 
-style = {
-    "font_family": "Comic Sans MS",
-    "font_size": "15px",
-}
+STYLESHEETS = [
+    "https://fonts.googleapis.com/css?family=Nunito+Sans",
+]
 
-app = rx.App(style=style)
+app = rx.App(stylesheets=STYLESHEETS)
 app.add_page(index, route="/", title="Kanghyun Kim")
 app.compile()
